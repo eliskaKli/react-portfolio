844d86125e5b03458d69efe876d83a668dd25115
eliskaKli <eliskaklimtova@seznam.cz>
2023-07-31 15:39:28 +0200

    First day changes - original

---
+++ src/components/ButtonPortfolio.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ButtonPortfolio() {
+    return(
+        <div 
+        // onClick={šoupne mě to na portfolio stránku} 
+        className='portfolioButton'>
+            <p>vstoupit do portfolia</p>
+        </div>    
+    )
+};
+
+export default ButtonPortfolio;
