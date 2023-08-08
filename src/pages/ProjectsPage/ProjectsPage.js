e563af982727a9e2d596eec9e6d1960d3c497702
eliskaKli <eliskaklimtova@seznam.cz>
2023-08-08 18:35:39 +0200

    Add ProjectPage responsivity a scrolling header

--- src/pages/ProjectsPage/ProjectsPage.js
+++ src/pages/ProjectsPage/ProjectsPage.js
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import styles from "./ProjectsPage.module.css";
 import ButtonContact from "../../components/ButtonContact/ButtonContact.js";
 import ProjectsList from "../../components/ProjectsList/ProjectsList.js";
 
-
 function ProjectsPage() {
-  const navigate= useNavigate();
-  const toProfilePage = () =>{
-    navigate("/")
+  // display border bottom line when scrolling
+  const [line, setLine] = useState(false);
+  const changeLine = () => {
+    if (window.scrollY >= 7.25) {
+      setLine(true);
+    } else {
+      setLine(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeLine);
+
+  // navigate to the profile page
+  const navigate = useNavigate();
+  const toProfilePage = () => {
+    navigate("/");
   };
 
   return (
     <div>
-      <header className={styles.header}>
+      <header
+        className={
+          line ? `${styles.header} ${styles.headerScroll}` : `${styles.header}`
+        }
+      >
         <p onClick={toProfilePage}>Eliška Klimtová</p>
         <ButtonContact className={styles.button} />
       </header>
-      <ProjectsList className={styles.projectsList}/>
+      <div className={styles.blankBar}></div>
+      <ProjectsList className={styles.projectsList} />
       <footer className={styles.footer}>
         {/* <a
           className={styles.linkPortfolioFigma}
