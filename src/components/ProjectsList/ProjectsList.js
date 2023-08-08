2e9c9c027d023473aabced5e7192705cf897ff17
eliskaKli <eliskaklimtova@seznam.cz>
2023-08-08 16:36:45 +0200

    Projects.json edit & desktop styling of ProjectsPg

--- src/components/ProjectsList/ProjectsList.js
+++ src/components/ProjectsList/ProjectsList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "./ProjectsList.module.css";
 import projects from "../../data/projects.json";
 
@@ -9,43 +9,42 @@ function ProjectsList() {
 
   return (
     <div className={styles.projectsContainer}>
-      {/* <div className={styles.tagsList}> */}
       {projects.map((item) => (
         <div className={styles.projectsList}>
           <img
             className={styles.projectImg}
-            // src={require(`../../data${item.img}`).default} 
+            // src={require(`../../data${item.img}`).default}
             src={process.env.PUBLIC_URL + item.img}
             alt={item.projectName}
           />
           <div className={styles.textContainer}>
-          <a
-            className={styles.h2}
-            href={item.src}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {item.projectName}
-            <FiExternalLink />
-          </a>
-          <p >{item.description}</p>
-          <a
-            className={styles.figmaLink}
-            href={item.figma}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Zobrazit Figmu
-          </a>
-          <ul className={styles.tagsContainer}>
-            {Array.isArray(item.tags) &&
-              item.tags.map((tag, i) => (
-                <li key={item.id} className={styles.tag}>
-                  {tag}
-                </li>
-              ))}
-          </ul>
-        </div>
+            <a
+              className={styles.h2}
+              href={item.src}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.projectName}
+              <FiExternalLink />
+            </a>
+            <p>{item.description}</p>
+            <a
+              className={styles.figmaLink}
+              href={item.figma}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.figma ? "Zobrazit Figmu" : null}
+            </a>
+            <ul className={styles.tagsContainer}>
+              {Array.isArray(item.tags) &&
+                item.tags.map((tag, i) => (
+                  <li key={tag.id} className={styles.tag}>
+                    {tag}
+                  </li>
+                ))}
+            </ul>
+          </div>
         </div>
       ))}
     </div>
--- src/pages/ProjectsPage/ProjectsPage.js
+++ src/pages/ProjectsPage/ProjectsPage.js
@@ -18,16 +18,16 @@ function ProjectsPage() {
         <p onClick={toProfilePage}>Eliška Klimtová</p>
         <ButtonContact className={styles.button} />
       </header>
-      <ProjectsList />
+      <ProjectsList className={styles.projectsList}/>
       <footer className={styles.footer}>
-        <a
+        {/* <a
           className={styles.linkPortfolioFigma}
           href="https://www.figma.com/file/r6PaxMek9So0hMPe3qp2UT/Portfolio-in-React?type=design&node-id=25%3A630&mode=design&t=v5oHb1dFHJClvcTo-1"
           target="_blank"
           rel="noopener noreferrer"
         >
           Prototyp tohoto portfolia navržený ve Figmě ke shlédnutí zde{" "}
-        </a>
+        </a> */}
         <p>Praha 2023</p>
       </footer>
     </div>
